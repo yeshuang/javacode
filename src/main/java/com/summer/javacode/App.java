41922226db6a2a374b80cd4c7d5d52a547de991d
yeshuang <ye.shuang520@163.com>
2015-09-17 02:47:49 +0800

    测试提交

--- src/main/java/com/summer/javacode/App.java
+++ src/main/java/com/summer/javacode/App.java
@@ -1,13 +1,13 @@
 package com.summer.javacode;
 
 /**
- * Hello world!
- *
+ * @ClassName: App
+ * @Description: TODO（这里用一句话描述这个类的作用）
+ * @author Shuang
+ * @date 2015年9月17日 上午2:46:55
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
